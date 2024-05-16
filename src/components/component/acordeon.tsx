455794a18a4c196d527a6a05ae8addb26d032cf3
jhowk14 <jonathanperozi16@gmail.com>
2024-05-16 14:54:25 -0300

    remove dark mode

--- src/components/component/acordeon.tsx
+++ src/components/component/acordeon.tsx
@@ -11,10 +11,10 @@ export function Acordeon({ items }: { items: AccordionData[] }) {
     <Accordion collapsible className="max-w-7xl w-full text-center" type="single">
       {items.map((item, index) => (
         <AccordionItem key={index} value={item.value}>
-          <AccordionTrigger className="flex items-center justify-between rounded-md bg-gray-100 px-4 py-3 lg:text-lg font-medium transition-colors hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700">
+          <AccordionTrigger className="flex items-center justify-between rounded-md bg-gray-100 px-4 py-3 lg:text-lg font-medium transition-colors hover:bg-gray-200">
             <span>{item.question}</span>
           </AccordionTrigger>
-          <AccordionContent className="space-y-4 rounded-b-md bg-yellowAcordeon-100 p-4 dark:bg-gray-800">
+          <AccordionContent className="space-y-4 rounded-b-md bg-yellowAcordeon-100 p-4">
             <p>{item.answer}</p>
           </AccordionContent>
         </AccordionItem>
