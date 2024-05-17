8aaeb6f28cdec6caf767283c443de11abcd549a3
jhowk14 <jonathanperozi16@gmail.com>
2024-05-17 14:16:31 -0300

    shadow and image alt fixed

--- src/app/components/card/cardContent2.tsx
+++ src/app/components/card/cardContent2.tsx
@@ -9,19 +9,19 @@ export default function CardContent2 (){
                     <Card 
                     text="Capacitação profissional (aulas de eletrônica básica com ênfase em hardware gratuitamente)."
                     image="img/cur.png"
-                    altt="icone curso"/>
+                    altt="icone computador desktop"/>
                     <Card
                     text="Proteção ao meio ambiente (descarte ecológico de materiais eletro/eletronico)." 
                     image="img/eco.png"
-                    altt="icone curso "/>
+                    altt="icone reciclagem"/>
                     <Card
                     text="Suporte técnico a entidades beneficientes (equipamentos)." 
                     image="img/doa.png"
-                    altt="icone curso"/>
+                    altt="icone doação"/>
                     <Card
                     text="Inclusão digital do deficiente visual (doação de computadores e periféricos)." 
                     image="img/vis.png"
-                    altt="icone curso"/>
+                    altt="icone deficiente visual"/>
                 </div>
             </div>
         </>
